a7b4bb56483f35ddc25adb56d5b3da4051bfd4a3
Andrew Speed <andrew.cspeed@gmail.com>
2024-11-16 22:44:57 +0000

    WIP: Potentially working binary tree solution?

--- src/huffman_tree.rs
+++ src/huffman_tree.rs
@@ -1,3 +1,5 @@
+use std::cmp::Ordering;
+
 trait Node<'a> {
     type Value;
 
@@ -26,6 +28,27 @@ impl BinaryTree for HuffmanTree<'_> {
     }
 }
 
+impl PartialEq for HuffmanTree<'_> {
+    fn eq(&self, other: &Self) -> bool {
+        self.root.clone().into_iter().eq(other.root.clone())
+    }
+}
+
+impl Eq for HuffmanTree<'_> {}
+
+impl PartialOrd for HuffmanTree<'_> {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for HuffmanTree<'_> {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.root.clone().into_iter().cmp(other.root.clone())
+    }
+}
+
+#[derive(Clone)]
 struct HuffmanTreeNode<'a> {
     left: Option<&'a Self>,
     right: Option<&'a Self>,
@@ -45,5 +68,31 @@ impl Node<'_> for HuffmanTreeNode<'_> {
 
     fn value(&self) -> &Self::Value {
         &self.value
+    }
+}
+
+impl PartialEq for HuffmanTreeNode<'_> {
+    fn eq(&self, other: &Self) -> bool {
+        self.value == other.value
+    }
+}
+
+impl Eq for HuffmanTreeNode<'_> {}
+
+impl PartialOrd for HuffmanTreeNode<'_> {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for HuffmanTreeNode<'_> {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        if self.value < other.value {
+            Ordering::Greater
+        } else if self.value == other.value {
+            Ordering::Equal
+        } else {
+            Ordering::Less
+        }
     }
 }
